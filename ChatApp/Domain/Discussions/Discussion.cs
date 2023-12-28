704e91a47bb3faeed324c06c1889ef52ebe4acee
WatIzDat <lily3575@hotmail.com>
2023-12-28 15:16:46 -0800

    Add Discussion aggregate root

---
+++ ChatApp/Domain/Discussions/Discussion.cs
@@ -0,0 +1,44 @@
+﻿using SharedKernel;
+
+namespace Domain.Discussions;
+
+public sealed class Discussion : Entity
+{
+    private Discussion(
+        Guid id,
+        Guid userCreatedBy,
+        string name,
+        DateTimeOffset dateCreatedUtc)
+        : base(id)
+    {
+        UserCreatedBy = userCreatedBy;
+        Name = name;
+        DateCreatedUtc = dateCreatedUtc;
+    }
+
+    public Guid UserCreatedBy { get; private set; }
+
+    public string Name { get; private set; }
+
+    public DateTimeOffset DateCreatedUtc { get; private set; }
+
+    public static Result<Discussion> Create(
+        Guid userCreatedBy,
+        string name,
+        DateTimeOffset dateCreatedUtc)
+    {
+        if (name.Length > 50)
+        {
+            return Result.Failure<Discussion>(DiscussionErrors.NameTooLong);
+        }
+
+        Discussion discussion = new(Guid.NewGuid(), userCreatedBy, name, dateCreatedUtc);
+
+        return Result.Success(discussion);
+    }
+
+    public void EditName(string name)
+    {
+        Name = name;
+    }
+}
---
+++ ChatApp/Domain/Discussions/DiscussionErrors.cs
@@ -0,0 +1,10 @@
+﻿using SharedKernel;
+
+namespace Domain.Discussions;
+
+public static class DiscussionErrors
+{
+    public static readonly Error NameTooLong = new(
+        "Discussion.NameTooLong",
+        "The name cannot be longer than 50 characters.");
+}
