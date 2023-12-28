5dd5adbe81af6a738f49d0db770b261d7b067745
WatIzDat <lily3575@hotmail.com>
2023-12-28 12:06:08 -0800

    Add abstract Entity class and domain event marker

---
+++ ChatApp/SharedKernel/Entity.cs
@@ -0,0 +1,30 @@
+﻿namespace SharedKernel;
+
+public abstract class Entity
+{
+    private readonly List<IDomainEvent> domainEvents = [];
+
+    protected Entity(Guid id)
+    {
+        Id = id;
+    }
+
+    protected Entity() 
+    {
+    }
+
+    public Guid Id { get; init; }
+
+    // ToList() makes copy instead of reference
+    public List<IDomainEvent> DomainEvents => domainEvents.ToList();
+
+    public void ClearDomainEvents()
+    {
+        domainEvents.Clear();
+    }
+
+    protected void RaiseDomainEvent(IDomainEvent domainEvent)
+    {
+        domainEvents.Add(domainEvent);
+    }
+}
