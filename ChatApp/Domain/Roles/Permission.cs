ac2d2eb4f30d3085bb0b580e80a85ec8f176e370
WatIzDat <lily3575@hotmail.com>
2023-12-28 22:11:13 -0800

    Add list of allowed permission values

--- ChatApp/Domain/Roles/Permission.cs
+++ ChatApp/Domain/Roles/Permission.cs
@@ -1,11 +1,38 @@
-﻿namespace Domain.Roles;
+﻿using SharedKernel;
+
+namespace Domain.Roles;
 
 public sealed record Permission
 {
-    public Permission(string value)
+    public static readonly Permission BanUser = new("user:ban");
+    public static readonly Permission KickUser = new("user:kick");
+    public static readonly Permission MuteUser = new("user:mute");
+    public static readonly Permission DeleteMessage = new("message:delete");
+
+    public static readonly List<Permission> AllowedPermissions =
+    [
+        BanUser,
+        KickUser,
+        MuteUser,
+        DeleteMessage
+    ];
+
+    private Permission(string value)
     {
         Value = value;
     }
 
     public string Value { get; }
+
+    public static Result<Permission> Create(string value)
+    {
+        Permission permission = new(value);
+
+        if (!AllowedPermissions.Contains(permission))
+        {
+            return Result.Failure<Permission>(PermissionErrors.NotAllowed);
+        }
+
+        return Result.Success(permission);
+    }
 }
---
+++ ChatApp/Domain/Roles/PermissionErrors.cs
@@ -0,0 +1,10 @@
+﻿using SharedKernel;
+
+namespace Domain.Roles;
+
+public class PermissionErrors
+{
+    public static readonly Error NotAllowed = new(
+        "Permission.NotAllowed",
+        "The provided permission is not in the list of allowed permissions.");
+}
