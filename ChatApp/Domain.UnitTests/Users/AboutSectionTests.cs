928d4beb62f998f40db1d84786e1919d17244c7f
WatIzDat <lily3575@hotmail.com>
2023-12-29 15:19:39 -0800

    Write AboutSection unit tests

---
+++ ChatApp/Domain.UnitTests/Users/AboutSectionTests.cs
@@ -0,0 +1,33 @@
+﻿using Domain.Users;
+using SharedKernel;
+
+namespace Domain.UnitTests.Users;
+
+public class AboutSectionTests
+{
+    [Fact]
+    public void Create_Should_ReturnSuccess()
+    {
+        Result<AboutSection> result = AboutSection.Create("This is a test.");
+
+        result.IsSuccess.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Create_Should_ReplaceLineEndings()
+    {
+        Result<AboutSection> result = AboutSection.Create("This is a test\n");
+
+        result.IsSuccess.Should().BeTrue();
+
+        result.Value.Should().Be(AboutSection.Create("This is a test\r\n").Value);
+    }
+
+    [Fact]
+    public void Create_Should_ReturnTooLong_WhenAboutSectionIsLongerThanMaxLength()
+    {
+        Result<AboutSection> result = AboutSection.Create("This about section is too long aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+
+        result.Error.Should().Be(AboutSectionErrors.TooLong);
+    }
+}
