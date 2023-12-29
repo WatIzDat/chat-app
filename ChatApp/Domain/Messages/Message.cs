3332ea4cdf1b5267899f256daebd549156f90bc1
WatIzDat <lily3575@hotmail.com>
2023-12-28 22:16:20 -0800

    Add Message aggregate root

---
+++ ChatApp/Domain/Messages/Message.cs
@@ -0,0 +1,58 @@
+﻿using SharedKernel;
+
+namespace Domain.Messages;
+
+public sealed class Message : Entity
+{
+    public const int ContentsMaxLength = 500;
+
+    private Message(
+        Guid id,
+        Guid userId,
+        Guid discussionId,
+        string contents,
+        DateTimeOffset dateSentUtc)
+        : base(id)
+    {
+        UserId = userId;
+        DiscussionId = discussionId;
+        Contents = contents;
+        DateSentUtc = dateSentUtc;
+    }
+
+    public Guid UserId { get; private set; }
+
+    public Guid DiscussionId { get; private set; }
+
+    public string Contents { get; private set; }
+
+    public DateTimeOffset DateSentUtc { get; private set; }
+
+    public static Result<Message> Create(
+        Guid userId,
+        Guid discussionId,
+        string contents,
+        DateTimeOffset dateSentUtc)
+    {
+        if (contents.Length > ContentsMaxLength)
+        {
+            return Result.Failure<Message>(MessageErrors.ContentsTooLong);
+        }
+
+        Message message = new(Guid.NewGuid(), userId, discussionId, contents, dateSentUtc);
+
+        return Result.Success(message);
+    }
+
+    public Result EditContents(string contents)
+    {
+        if (contents.Length > ContentsMaxLength)
+        {
+            return Result.Failure(MessageErrors.ContentsTooLong);
+        }
+
+        Contents = contents;
+
+        return Result.Success();
+    }
+}
---
+++ ChatApp/Domain/Messages/MessageErrors.cs
@@ -0,0 +1,10 @@
+﻿using SharedKernel;
+
+namespace Domain.Messages;
+
+public class MessageErrors
+{
+    public static readonly Error ContentsTooLong = new(
+        "Message.ContentsTooLong",
+        $"The contents cannot be longer than {Message.ContentsMaxLength} characters.");
+}
