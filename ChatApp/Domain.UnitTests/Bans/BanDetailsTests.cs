a693fe7792788769ca98c57332a093dcb192acc0
WatIzDat <lily3575@hotmail.com>
2023-12-29 18:31:59 -0800

    Write BanDetails unit tests

---
+++ ChatApp/Domain.UnitTests/Bans/BanDetailsTests.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Bans;
+
+namespace Domain.UnitTests.Bans;
+
+public class BanDetailsTests
+{
+    [Fact]
+    public void CreateTemporaryBan_Should_ReturnBanDetails_WhereIsBanPermanentIsFalse_And_DateOfUnbanIsSet()
+    {
+        DateTimeOffset utcNow = DateTimeOffset.UtcNow;
+
+        BanDetails banDetails = BanDetails.CreateTemporaryBan(utcNow);
+
+        banDetails.IsBanPermanent.Should().BeFalse();
+        banDetails.DateOfUnbanUtc.Should().Be(utcNow);
+    }
+
+    [Fact]
+    public void CreatePermanentBan_Should_ReturnBanDetails_WhereIsBanPermanentIsTrue_And_DateOfUnbanIsNull()
+    {
+        BanDetails banDetails = BanDetails.CreatePermanentBan();
+
+        banDetails.IsBanPermanent.Should().BeTrue();
+        banDetails.DateOfUnbanUtc.Should().BeNull();
+    }
+}
