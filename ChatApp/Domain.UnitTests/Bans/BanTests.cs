fe2078ef10972fbae24c7f4879af83603d98ed2f
WatIzDat <lily3575@hotmail.com>
2023-12-29 18:23:06 -0800

    Write Ban unit tests

---
+++ ChatApp/Domain.UnitTests/Bans/BanTests.cs
@@ -0,0 +1,37 @@
+﻿using Domain.Bans;
+using SharedKernel;
+
+namespace Domain.UnitTests.Bans;
+
+public class BanTests
+{
+    [Fact]
+    public void UnbanUser_Should_ReturnSuccess_And_SetIsUnbannedToTrue()
+    {
+        Ban ban = Ban.Create(
+            Guid.NewGuid(),
+            Guid.NewGuid(),
+            DateTimeOffset.UtcNow,
+            BanDetails.CreatePermanentBan());
+
+        Result result = ban.UnbanUser(DateTimeOffset.UtcNow);
+
+        result.IsSuccess.Should().BeTrue();
+
+        ban.IsUnbanned.Should().BeTrue();
+    }
+
+    [Fact]
+    public void UnbanUser_Should_ReturnCurrentTimeEarlierThanDateOfUnban_WhenUtcNowIsEarlierThanDateOfUnban()
+    {
+        Ban ban = Ban.Create(
+            Guid.NewGuid(),
+            Guid.NewGuid(),
+            DateTimeOffset.UtcNow,
+            BanDetails.CreateTemporaryBan(DateTimeOffset.UtcNow.AddDays(1)));
+
+        Result result = ban.UnbanUser(DateTimeOffset.UtcNow);
+
+        result.Error.Should().Be(BanErrors.CurrentTimeEarlierThanDateOfUnban);
+    }
+}
