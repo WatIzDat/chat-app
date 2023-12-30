84603787bc42abc336d802d1e37e9afb43e81d85
WatIzDat <lily3575@hotmail.com>
2023-12-29 17:51:24 -0800

    Write Permission unit tests

---
+++ ChatApp/Domain.UnitTests/Roles/PermissionTests.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Roles;
+using SharedKernel;
+
+namespace Domain.UnitTests.Roles;
+
+public class PermissionTests
+{
+    [Fact]
+    public void Create_Should_ReturnSuccess()
+    {
+        Result<Permission> result = Permission.Create("user:ban");
+
+        result.IsSuccess.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Create_Should_ReturnNotAllowed_WhenAllowedPermissionsDoesNotContainPermission()
+    {
+        Result<Permission> result = Permission.Create("not allowed");
+
+        result.Error.Should().Be(PermissionErrors.NotAllowed);
+    }
+}
