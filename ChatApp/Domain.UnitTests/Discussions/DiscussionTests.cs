9113bce0570050da165266a8a47b58095ba74454
WatIzDat <lily3575@hotmail.com>
2023-12-29 18:11:43 -0800

    Write Discussion unit tests

---
+++ ChatApp/Domain.UnitTests/Discussions/DiscussionTests.cs
@@ -0,0 +1,52 @@
+﻿using Domain.Discussions;
+using SharedKernel;
+
+namespace Domain.UnitTests.Discussions;
+
+public class DiscussionTests
+{
+    private static readonly Discussion Discussion = Discussion.Create(
+        Guid.NewGuid(),
+        "test",
+        DateTimeOffset.UtcNow).Value;
+
+    [Fact]
+    public void Create_Should_ReturnSuccess()
+    {
+        Result<Discussion> result = Discussion.Create(
+            Discussion.UserCreatedBy,
+            Discussion.Name,
+            Discussion.DateCreatedUtc);
+
+        result.IsSuccess.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Create_Should_ReturnNameTooLong_WhenNameIsLongerThanMaxLength()
+    {
+        Result<Discussion> result = Discussion.Create(
+            Discussion.UserCreatedBy,
+            "this name is too longaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+            Discussion.DateCreatedUtc);
+
+        result.Error.Should().Be(DiscussionErrors.NameTooLong);
+    }
+
+    [Fact]
+    public void EditName_Should_ReturnSuccess_And_ChangeName()
+    {
+        Result result = Discussion.EditName("hello");
+
+        result.IsSuccess.Should().BeTrue();
+
+        Discussion.Name.Should().Be("hello");
+    }
+
+    [Fact]
+    public void EditName_Should_ReturnNameTooLong_WhenNameIsLongerThanMaxLength()
+    {
+        Result result = Discussion.EditName("this name is too longaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+
+        result.Error.Should().Be(DiscussionErrors.NameTooLong);
+    }
+}
