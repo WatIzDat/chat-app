5862ef8779d1168d83c6d5324def9501ad16077d
WatIzDat <lily3575@hotmail.com>
2023-12-29 18:02:10 -0800

    Write Message unit tests

---
+++ ChatApp/Domain.UnitTests/Messages/MessageTests.cs
@@ -0,0 +1,69 @@
+﻿using Domain.Messages;
+using SharedKernel;
+
+namespace Domain.UnitTests.Messages;
+
+public class MessageTests
+{
+    private static readonly Message Message = Message.Create(
+        Guid.NewGuid(),
+        Guid.NewGuid(),
+        "This is a message.",
+        DateTimeOffset.UtcNow).Value;
+
+    [Fact]
+    public void Create_Should_ReturnSuccess()
+    {
+        Result<Message> result = Message.Create(
+            Message.UserId,
+            Message.DiscussionId,
+            Message.Contents,
+            Message.DateSentUtc);
+
+        result.IsSuccess.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Create_Should_ReturnContentsTooLong_WhenContentsIsLongerThanMaxLength()
+    {
+        string contents = string.Empty;
+
+        for (int i = 0; i < Message.ContentsMaxLength + 1; i++)
+        {
+            contents += "a";
+        }
+
+        Result<Message> result = Message.Create(
+            Message.UserId,
+            Message.DiscussionId,
+            contents,
+            Message.DateSentUtc);
+
+        result.Error.Should().Be(MessageErrors.ContentsTooLong);
+    }
+
+    [Fact]
+    public void EditContents_Should_ReturnSuccess_And_ChangeContents()
+    {
+        Result result = Message.EditContents("hello");
+
+        result.IsSuccess.Should().BeTrue();
+
+        Message.Contents.Should().Be("hello");
+    }
+
+    [Fact]
+    public void EditContents_Should_ReturnContentsTooLong_WhenContentsIsLongerThanMaxLength()
+    {
+        string contents = string.Empty;
+
+        for (int i = 0; i < Message.ContentsMaxLength + 1; i++)
+        {
+            contents += "a";
+        }
+
+        Result result = Message.EditContents(contents);
+
+        result.Error.Should().Be(MessageErrors.ContentsTooLong);
+    }
+}
