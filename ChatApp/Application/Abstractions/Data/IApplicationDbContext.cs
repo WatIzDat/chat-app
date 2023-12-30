e8d736450848e8bdce6f5ac22c550dbc12b28534
WatIzDat <lily3575@hotmail.com>
2023-12-29 19:31:24 -0800

    Add ApplicationDbContext and UnitOfWork interfaces

---
+++ ChatApp/Application/Abstractions/Data/IApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Bans;
+using Domain.Discussions;
+using Domain.Messages;
+using Domain.Roles;
+using Domain.Users;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Abstractions.Data;
+
+public interface IApplicationDbContext
+{
+    DbSet<Ban> Bans { get; set; }
+
+    DbSet<Discussion> Discussions { get; set; }
+
+    DbSet<Message> Messages { get; set; }
+
+    DbSet<Role> Roles { get; set; }
+
+    DbSet<User> Users { get; set; }
+}
---
+++ ChatApp/Application/Abstractions/Data/IUnitOfWork.cs
@@ -0,0 +1,6 @@
+﻿namespace Application.Abstractions.Data;
+
+public interface IUnitOfWork
+{
+    Task SaveChangesAsync(CancellationToken cancellationToken = default);
+}
