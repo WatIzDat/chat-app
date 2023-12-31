de4df78964378160aa786ce1d03d9e819d46a9f5
WatIzDat <lily3575@hotmail.com>
2023-12-30 19:04:13 -0800

    Add ChangeEmailCommand and command handler

---
+++ ChatApp/Application/Users/ChangeEmail/ChangeEmailCommand.cs
@@ -0,0 +1,5 @@
+﻿using Application.Abstractions.Messaging;
+
+namespace Application.Users.ChangeEmail;
+
+public sealed record ChangeEmailCommand(Guid UserId, string Email) : ICommand;
---
+++ ChatApp/Application/Users/ChangeEmail/ChangeEmailCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using Application.Abstractions.Messaging;
+using Domain.Users;
+using SharedKernel;
+
+namespace Application.Users.ChangeEmail;
+
+internal sealed class ChangeEmailCommandHandler(IUserRepository userRepository)
+    : ICommandHandler<ChangeEmailCommand>
+{
+    private readonly IUserRepository userRepository = userRepository;
+
+    public async Task<Result> Handle(ChangeEmailCommand request, CancellationToken cancellationToken)
+    {
+        User? user = await userRepository.GetByIdAsync(request.UserId, cancellationToken);
+
+        if (user == null)
+        {
+            return Result.Failure(UserErrors.NotFound);
+        }
+
+        Result<Email> emailResult = Email.Create(request.Email);
+
+        if (emailResult.IsFailure)
+        {
+            return Result.Failure(emailResult.Error);
+        }
+
+        Email email = emailResult.Value;
+
+        user.ChangeEmail(email);
+
+        userRepository.Update(user);
+
+        return Result.Success();
+    }
+}
