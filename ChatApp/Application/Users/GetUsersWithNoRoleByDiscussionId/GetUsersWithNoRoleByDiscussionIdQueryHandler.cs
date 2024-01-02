8546b104ba36541cf867866ce5106cfa8e6f1c01
WatIzDat <lily3575@hotmail.com>
2024-01-02 15:28:54 -0800

    Fix GetUsersWithNoRoleByDiscussionIdQueryHandler
    Do check to make sure users have no role and improve readability

--- ChatApp/Application/Users/GetUsersWithNoRoleByDiscussionId/GetUsersWithNoRoleByDiscussionIdQueryHandler.cs
+++ ChatApp/Application/Users/GetUsersWithNoRoleByDiscussionId/GetUsersWithNoRoleByDiscussionIdQueryHandler.cs
@@ -1,5 +1,6 @@
 using Application.Abstractions.Data;
 using Application.Abstractions.Messaging;
+using Domain.Users;
 using Microsoft.EntityFrameworkCore;
 using SharedKernel;
 
@@ -12,8 +13,13 @@ internal sealed class GetUsersWithNoRoleByDiscussionIdQueryHandler(IApplicationD
 
     public async Task<Result<List<UserResponse>>> Handle(GetUsersWithNoRoleByDiscussionIdQuery request, CancellationToken cancellationToken)
     {
-        List<UserResponse> users = await dbContext.Users
-            .Where(u => u.Discussions.Value.Contains(request.DiscussionId))
+        IQueryable<User> usersInDiscussion = dbContext.Users
+            .Where(u => u.Discussions.Value.Contains(request.DiscussionId));
+
+        IQueryable<User> usersInDiscussionWithNoRole = usersInDiscussion 
+            .Where(u => u.Roles.Value.All(id => dbContext.Roles.Where(r => r.Id == id).Any(r => r.DiscussionId != request.DiscussionId)));
+
+        IQueryable<UserResponse> mappedUsers = usersInDiscussionWithNoRole
             .Select(u => new UserResponse
             {
                 Id = u.IsDeleted ? Guid.Empty : u.Id,
@@ -21,15 +27,17 @@ internal sealed class GetUsersWithNoRoleByDiscussionIdQueryHandler(IApplicationD
                 Email = u.IsDeleted ? "" : u.Email.Value,
                 DateCreatedUtc = u.IsDeleted ? DateTimeOffset.MinValue : u.DateCreatedUtc,
                 AboutSection = u.IsDeleted ? "" : u.AboutSection.Value
-            })
-            // Pagination
+            });
+
+        IQueryable<UserResponse> paginatedUsers = mappedUsers
             .OrderBy(u => u.DateCreatedUtc)
             .ThenBy(u => u.Id)
             .Where(u => u.DateCreatedUtc > request.LastDateCreatedUtc
                     || (u.DateCreatedUtc == request.LastDateCreatedUtc && u.Id > request.LastUserId))
-            .Take(request.Limit)
-            .ToListAsync(cancellationToken);
+            .Take(request.Limit);
 
-        return Result.Success(users);
+        List<UserResponse> finalUsersResult = await paginatedUsers.ToListAsync(cancellationToken);
+
+        return Result.Success(finalUsersResult);
     }
 }
