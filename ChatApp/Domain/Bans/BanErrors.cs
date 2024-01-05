312ddf17ab7308944f0307cfbeab0404388b0038
WatIzDat <lily3575@hotmail.com>
2024-01-04 19:14:55 -0800

    Add UserAlreadyBannedFromDiscussion ban error
    Add BanExistsByUserAndDiscussionIdAsync method in ban repository

--- ChatApp/Domain/Bans/BanErrors.cs
+++ ChatApp/Domain/Bans/BanErrors.cs
@@ -7,4 +7,8 @@ public static class BanErrors
     public static readonly Error CurrentTimeEarlierThanDateOfUnban = new(
         "Ban.CurrentTimeEarlierThanDateOfUnban",
         "The current time is earlier than the specified date of unban.");
+
+    public static readonly Error UserAlreadyBannedFromDiscussion = new(
+        "Ban.UserAlreadyBannedFromDiscussion",
+        "The requested user has already been banned from the specified discussion.");
 }
--- ChatApp/Domain/Bans/IBanRepository.cs
+++ ChatApp/Domain/Bans/IBanRepository.cs
@@ -3,4 +3,6 @@ namespace Domain.Bans;
 public interface IBanRepository
 {
     void Insert(Ban ban);
+
+    Task<bool> BanExistsByUserAndDiscussionIdAsync(Guid userId, Guid discussionId, CancellationToken cancellationToken = default);
 }
