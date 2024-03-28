7aac24d58fcbbe1f9955fe9f67bdd9ff2a7fcb93
WatIzDat <lily3575@hotmail.com>
2024-03-28 16:22:07 -0700

    Add web API project and dependency injection
    Uses Scrutor for dependency injection

---
+++ ChatApp/Web.Api/Program.cs
@@ -0,0 +1,35 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder
+    .Services
+    .Scan(
+        selector => selector
+            .FromAssemblies(
+                Infrastructure.AssemblyReference.Assembly)
+            .AddClasses(false)
+            .AsImplementedInterfaces()
+            .WithScopedLifetime());
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
