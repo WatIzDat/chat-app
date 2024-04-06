379a9bc2baf334a17c778f0e91ca923cf30af5a7
WatIzDat <lily3575@hotmail.com>
2024-04-06 14:39:36 -0700

    Write UnbanUserCommand unit tests

---
+++ ChatApp/Application.UnitTests/Bans/UnbanUserCommandTests.cs
@@ -0,0 +1,48 @@
+﻿using Application.Bans.UnbanUser;
+using Domain.Bans;
+using NSubstitute.ReturnsExtensions;
+using SharedKernel;
+
+namespace Application.UnitTests.Bans;
+
+public class UnbanUserCommandTests
+{
+    private static readonly UnbanUserCommand Command = new(Guid.NewGuid());
+
+    private static readonly Ban Ban = Ban.Create(
+        Command.UserId,
+        Guid.NewGuid(),
+        DateTimeOffset.UtcNow,
+        BanDetails.CreateTemporaryBan(DateTimeOffset.UtcNow, DateTime.UtcNow.AddDays(1)).Value);
+
+    private readonly UnbanUserCommandHandler commandHandler;
+
+    private readonly IBanRepository banRepositoryMock;
+
+    public UnbanUserCommandTests()
+    {
+        banRepositoryMock = Substitute.For<IBanRepository>();
+
+        commandHandler = new UnbanUserCommandHandler(banRepositoryMock);
+    }
+
+    [Fact]
+    public async Task Handle_Should_ReturnSuccess()
+    {
+        banRepositoryMock.GetByUserIdAsync(Arg.Is(Command.UserId)).Returns(Ban);
+
+        Result result = await commandHandler.Handle(Command, default);
+
+        result.IsSuccess.Should().BeTrue();
+    }
+
+    [Fact]
+    public async Task Handle_Should_ReturnBanNotFoundByUserId_WhenGetByUserIdAsyncReturnsNull()
+    {
+        banRepositoryMock.GetByUserIdAsync(Arg.Is(Command.UserId)).ReturnsNull();
+
+        Result result = await commandHandler.Handle(Command, default);
+
+        result.Error.Should().Be(BanErrors.NotFoundByUserId);
+    }
+}
