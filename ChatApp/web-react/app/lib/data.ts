6528f1031bba3993fafe7238769b6de9be0ff4db
WatIzDat <lily3575@hotmail.com>
2024-05-06 21:11:47 -0700

    Implement middleware redirect for discussion pages

--- ChatApp/web-react/app/lib/data.ts
+++ ChatApp/web-react/app/lib/data.ts
@@ -16,3 +16,29 @@ export async function fetchUserById(id: string) {
         throw new Error("Failed to fetch user");
     }
 }
+
+export async function fetchCreatedDiscussionsByUser(userId: string) {
+    try {
+        const response = await fetch(
+            `http://localhost:8080/discussions/get-created-discussions-by-user?userId=${userId}`
+        );
+
+        return await response.json();
+    } catch (error) {
+        console.error("Error fetching discussions", error);
+        throw new Error("Failed to fetch discussions");
+    }
+}
+
+export async function fetchJoinedDiscussionsByUser(userId: string) {
+    try {
+        const response = await fetch(
+            `http://localhost:8080/discussions/get-joined-discussions-by-user?userId=${userId}`
+        );
+
+        return await response.json();
+    } catch (error) {
+        console.error("Error fetching discussions", error);
+        throw new Error("Failed to fetch discussions");
+    }
+}
--- ChatApp/web-react/middleware.ts
+++ ChatApp/web-react/middleware.ts
@@ -1,10 +1,54 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { fetchJoinedDiscussionsByUser } from "@/app/lib/data";
+import { NextResponse } from "next/server";
 
-const isProtectedRoute = createRouteMatcher(["/my-discussions(.*)"]);
+const isProtectedRoute = createRouteMatcher([
+    "/my-discussions(.*)",
+    "/discussion/(.*)",
+    "/join-discussion/(.*)",
+]);
 
-export default clerkMiddleware((auth, req) => {
+const isDiscussionRoute = createRouteMatcher(["/discussion/(.*)"]);
+
+const isJoinDiscussionRoute = createRouteMatcher(["/join-discussion/(.*)"]);
+
+export default clerkMiddleware(async (auth, req) => {
     if (isProtectedRoute(req)) {
         auth().protect();
+    }
+
+    if (isDiscussionRoute(req)) {
+        const urlSplit = req.url.split("/");
+        const discussionId = urlSplit[urlSplit.length - 1];
+
+        const joinedDiscussions: { id: string; name: string }[] =
+            await fetchJoinedDiscussionsByUser(
+                auth().sessionClaims?.userId as string
+            );
+
+        if (!joinedDiscussions.map((d) => d.id).includes(discussionId)) {
+            const url = req.nextUrl.clone();
+            url.pathname = `join-discussion/${discussionId}`;
+
+            return NextResponse.redirect(url);
+        }
+    }
+
+    if (isJoinDiscussionRoute(req)) {
+        const urlSplit = req.url.split("/");
+        const discussionId = urlSplit[urlSplit.length - 1];
+
+        const joinedDiscussions: { id: string; name: string }[] =
+            await fetchJoinedDiscussionsByUser(
+                auth().sessionClaims?.userId as string
+            );
+
+        if (joinedDiscussions.map((d) => d.id).includes(discussionId)) {
+            const url = req.nextUrl.clone();
+            url.pathname = `discussion/${discussionId}`;
+
+            return NextResponse.redirect(url);
+        }
     }
 });
 
