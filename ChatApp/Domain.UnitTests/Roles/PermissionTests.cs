6eab3c57e1b0301dc9d6261124248e0130f6e081
WatIzDat <lily3575@hotmail.com>
2024-05-18 12:33:55 -0700

    Update role tests to follow better practices

--- ChatApp/Domain.UnitTests/Roles/PermissionTests.cs
+++ ChatApp/Domain.UnitTests/Roles/PermissionTests.cs
@@ -8,16 +8,26 @@ public class PermissionTests
     [Fact]
     public void Create_Should_ReturnSuccess()
     {
-        Result<Permission> result = Permission.Create("user:ban");
+        // Arrange
+        string validValue = "user:ban";
 
+        // Act
+        Result<Permission> result = Permission.Create(validValue);
+
+        // Assert
         result.IsSuccess.Should().BeTrue();
     }
 
     [Fact]
     public void Create_Should_ReturnNotAllowed_WhenAllowedPermissionsDoesNotContainPermission()
     {
-        Result<Permission> result = Permission.Create("not allowed");
+        // Arrange
+        string invalidValue = string.Empty;
 
+        // Act
+        Result<Permission> result = Permission.Create(invalidValue);
+
+        // Assert
         result.Error.Should().Be(PermissionErrors.NotAllowed);
     }
 }
--- ChatApp/Domain.UnitTests/Roles/RoleTests.cs
+++ ChatApp/Domain.UnitTests/Roles/RoleTests.cs
@@ -5,88 +5,175 @@ namespace Domain.UnitTests.Roles;
 
 public class RoleTests
 {
-    private static readonly List<Permission> Permissions = [Permission.BanUser, Permission.DeleteMessage];
-
-    private static readonly Role Role = Role.Create(Guid.NewGuid(), "test", Permissions).Value;
+    private static Role CreateDefaultRole()
+    {
+        return Role.Create(Guid.Empty, "test", []).Value;
+    }
 
     [Fact]
     public void Create_Should_ReturnSuccess()
     {
-        Result<Role> result = Role.Create(Role.DiscussionId, Role.Name, Permissions);
+        // Arrange
+        Role referenceRole = CreateDefaultRole();
 
+        // Act
+        Result<Role> result = Role.Create(
+            referenceRole.DiscussionId,
+            referenceRole.Name,
+            referenceRole.Permissions);
+
+        // Assert
         result.IsSuccess.Should().BeTrue();
     }
 
     [Fact]
     public void Create_Should_ReturnNameTooLong_WhenNameIsLongerThanMaxLength()
     {
-        Result<Role> result = Role.Create(Role.DiscussionId, "thisnameiswaytoolongaaaaaaaaaaaaaaa", Permissions);
+        // Arrange
+        Role referenceRole = CreateDefaultRole();
 
+        string longerThanMaxLength = string.Empty.PadLeft(Role.NameMaxLength + 1);
+
+        // Act
+        Result<Role> result = Role.Create(
+            referenceRole.DiscussionId,
+            longerThanMaxLength,
+            referenceRole.Permissions);
+
+        // Assert
         result.Error.Should().Be(RoleErrors.NameTooLong);
     }
 
     [Fact]
     public void Create_Should_ReturnDuplicatePermissions_WhenDuplicatePermissionsAreInList()
     {
-        Result<Role> result = Role.Create(
-            Role.DiscussionId,
-            Role.Name,
-            [Permission.BanUser, Permission.BanUser]);
+        // Arrange
+        Role referenceRole = CreateDefaultRole();
 
+        List<Permission> hasDuplicatePermissions = [Permission.BanUser, Permission.BanUser];
+
+        // Act
+        Result<Role> result = Role.Create(
+            referenceRole.DiscussionId,
+            referenceRole.Name,
+            hasDuplicatePermissions);
+
+        // Assert
         result.Error.Should().Be(RoleErrors.DuplicatePermissions);
     }
 
     [Fact]
-    public void EditName_Should_ReturnSuccess_And_ChangeName()
+    public void EditName_Should_ReturnSuccess()
     {
-        Result result = Role.EditName("hello");
+        // Arrange
+        Role defaultRole = CreateDefaultRole();
 
+        string validName = "a";
+
+        // Act
+        Result result = defaultRole.EditName(validName);
+
+        // Assert
         result.IsSuccess.Should().BeTrue();
-
-        Role.Name.Should().Be("hello");
     }
 
     [Fact]
+    public void EditName_Should_ChangeName()
+    {
+        // Arrange
+        Role defaultRole = CreateDefaultRole();
+
+        string validName = "a";
+
+        // Act
+        defaultRole.EditName(validName);
+
+        // Assert
+        defaultRole.Name.Should().Be(validName);
+    }
+    [Fact]
     public void EditName_Should_ReturnNameTooLong_WhenNameIsLongerThanMaxLength()
     {
-        Result result = Role.EditName("thisnameiswaytoolongaaaaaaaaaaaaaaa");
+        // Arrange
+        Role defaultRole = CreateDefaultRole();
 
+        string longerThanMaxLength = string.Empty.PadLeft(Role.NameMaxLength + 1);
+
+        // Act
+        Result result = defaultRole.EditName(longerThanMaxLength);
+
+        // Assert
         result.Error.Should().Be(RoleErrors.NameTooLong);
     }
 
     [Fact]
-    public void AddPermission_Should_ReturnSuccess_And_AddPermissionToList()
+    public void AddPermission_Should_ReturnSuccess()
     {
-        Result result = Role.AddPermission(Permission.KickUser);
+        // Arrange
+        Role defaultRole = CreateDefaultRole();
 
+        // Act
+        Result result = defaultRole.AddPermission(Permission.KickUser);
+
+        // Assert
         result.IsSuccess.Should().BeTrue();
+    }
 
-        Role.Permissions.Should().Contain(Permission.KickUser);
+    [Fact]
+    public void AddPermission_Should_AddPermissionToList()
+    {
+        // Arrange
+        Role defaultRole = CreateDefaultRole();
+
+        // Act
+        defaultRole.AddPermission(Permission.KickUser);
+
+        // Assert
+        defaultRole.Permissions.Should().Contain(Permission.KickUser);
     }
 
     [Fact]
     public void AddPermission_Should_ReturnDuplicatePermissions_WhenDuplicatePermissionIsAdded()
     {
-        Role.AddPermission(Permission.KickUser);
+        // Arrange
+        Role defaultRole = CreateDefaultRole();
 
-        Result result = Role.AddPermission(Permission.KickUser);
+        defaultRole.AddPermission(Permission.KickUser);
 
+        // Act
+        Result result = defaultRole.AddPermission(Permission.KickUser);
+
+        // Assert
         result.Error.Should().Be(RoleErrors.DuplicatePermissions);
     }
 
     [Fact]
     public void RemovePermission_Should_RemovePermission()
     {
-        Role.RemovePermission(Permission.BanUser);
+        // Arrange
+        Role defaultRole = CreateDefaultRole();
 
-        Role.Permissions.Should().NotContain(Permission.BanUser);
+        defaultRole.AddPermission(Permission.BanUser);
+
+        // Act
+        defaultRole.RemovePermission(Permission.BanUser);
+
+        // Assert
+        defaultRole.Permissions.Should().NotContain(Permission.BanUser);
     }
 
     [Fact]
     public void RemovePermission_Should_ReturnPermissionNotFound_WhenPermissionCouldNotBeRemoved()
     {
-        Result result = Role.RemovePermission(Permission.MuteUser);
+        // Arrange
+        Role defaultRole = CreateDefaultRole();
 
+        Permission permissionNotInList = Permission.MuteUser;
+
+        // Act
+        Result result = defaultRole.RemovePermission(permissionNotInList);
+
+        // Assert
         result.Error.Should().Be(RoleErrors.PermissionNotFound);
     }
 }
